50639a5f7de191014fe5e6016472b2f4d5f8678d
Alvaro <32452108+Alvarohao@users.noreply.github.com>
2022-03-19 22:52:06 +0800

    Add files via upload

---
+++ demo_01/main.c
@@ -0,0 +1,25 @@
+//# 预处理指令（车钥匙） include 包含 <stdio.h> .h head 头文件 
+//stdio std standard 标准 i input 输入 o out 输出
+
+//这个文件可能会包含一个标准输入输出的头文件
+
+#include<stdio.h>
+
+/*main()为函数名
+void表示mian()不带任何参数
+int表明main()函数返回一个整数*/
+
+int main(void)
+{	//函数体开始
+
+	/*我的第一个 c 程序 注释：不被编译器识别*/
+	//注释
+
+	printf("Hello world!\n");	//调用函数printf
+
+	/*print 打印 f format 格式化
+	printf 格式化输出*/
+
+	return 0;
+
+}	//结束
